4dbd3dac12b12b59da6472ac16f4e9bd30e10986
sarathsp06 <sarath.sp06@gmail.com>
2015-12-08 16:54:52 +0530

    Added an example with a running server code

---
+++ example/server/server.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/exotel/goexoml"
+	"github.com/labstack/echo"
+	mw "github.com/labstack/echo/middleware"
+)
+
+//Dial returns a twniml given number to call a number
+func Dial(c *echo.Context) error {
+	var number string
+	number = c.Param("number")
+	if number == "" {
+		return c.String(http.StatusBadRequest, "PROVIDE NUMBER TO BE CALLED")
+	}
+	resp := goexoml.NewResponse()
+	say := goexoml.NewSay().SetText("You can't handle the truth")
+	dial := goexoml.NewDial().SetPlainNumber(number) //SetNumber(*(goexoml.NewNumber().SetNoun(number)))
+	resp.AddSay(say).AddDial(dial).AddHangup(goexoml.NewHangup())
+	return c.XML(http.StatusOK, resp)
+}
+
+func main() {
+	// Echo instance
+	e := echo.New()
+	// Middleware
+	e.Use(mw.Logger())
+	e.Use(mw.Recover())
+
+	// Routes
+	e.Get("/dial/:number", Dial)
+	// Start server
+	e.Run(":1323")
+}
